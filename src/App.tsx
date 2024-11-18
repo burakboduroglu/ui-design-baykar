b6116d22ec02ae53152995c32004939315c52f43
burakboduroglu <burakboduroglu@outlook.com>
2024-11-18 15:16:14 +0300

    docs: :memo: readme description

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import Header from "./components/Header/Header.tsx";
-import Hero from "./components/Hero/Hero.tsx";
-import Shop from "./components/Shop/Shop.tsx";
-import About from "./components/About/About.tsx";
-import Comment from "./components/Comment/Comment.tsx";
+import Header from './components/Header/Header.tsx';
+import Hero from './components/Hero/Hero.tsx';
+import Shop from './components/Shop/Shop.tsx';
+import About from './components/About/About.tsx';
+import Comment from './components/Comment/Comment.tsx';
 
 function App() {
-    return (
-        <div className="bg-[#FDF9F3] min-h-screen font-sans">
-            <Header />
-            <Hero />
-            <div id="shop">
-                <Shop />
-            </div>
-            <About />
-            <Comment />
-        </div>
-    );
+  return (
+    <div className='bg-[#FDF9F3] min-h-screen font-sans'>
+      <Header />
+      <Hero />
+      <div id='shop'>
+        <Shop />
+      </div>
+      <About />
+      <Comment />
+    </div>
+  );
 }
 
 export default App;
